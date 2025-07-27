d913dbdd8911b371d9be41bcc0e5f9591ffc51e6
Thantulage Jayathu Chankama Fernando <155064611+dataemperor@users.noreply.github.com>
2025-07-28 02:22:30 +0530

    Implemented Program class

---
+++ student_mentor_management_system/src/main/java/com/github/dataemperor/student_mentor_management_system/classes/Program.java
@@ -0,0 +1,12 @@
+package com.github.dataemperor.student_mentor_management_system.classes;
+
+public class Program {
+
+    private String programId;
+    private String programName;
+
+    public Program(String programId, String programName) {
+        this.programId = programId;
+        this.programName = programName;
+    }
+}
