079ef0bc9a6b9401acd4dfe92f5096587b8344e4
Thantulage Jayathu Chankama Fernando <155064611+dataemperor@users.noreply.github.com>
2025-07-30 01:41:56 +0530

    Implementated Database class with a constructor that created a database if it isn't present, connects to it and two other methods for initializing the student and program tables and a close connection method to be used in the future

---
+++ student_mentor_management_system/src/main/java/com/github/dataemperor/student_mentor_management_system/database/Database.java
@@ -0,0 +1,78 @@
+package com.github.dataemperor.student_mentor_management_system.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+
+    private Connection connection = null;
+
+    public Database() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection(
+                "jdbc:sqlite:ManagementSystemDatabase.db"
+            );
+            try (Statement statement = connection.createStatement()) {
+                statement.execute("PRAGMA foreign_keys = ON;");
+            }
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+            System.exit(1);
+        } catch (ClassNotFoundException e) {
+            System.err.println("ClassNotFoundException: " + e.getMessage());
+            System.exit(1);
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+
+    public void closeConnection() {
+        try {
+            connection.close();
+            connection = null; // preventing reuse
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+        }
+    }
+
+    public void initializeTables() {
+        if (connection == null) {
+            System.err.println("Connection is null, cannot initalize tables");
+            System.exit(1);
+        }
+        String sqlProgram =
+            "CREATE TABLE IF NOT EXISTS PROGRAM (" +
+            "program_id VARCHAR(10) UNIQUE NOT NULL," +
+            "program_name VARCHAR(30) NOT NULL," +
+            "PRIMARY KEY (program_id)" +
+            " )";
+        String sqlStudent =
+            "CREATE TABLE IF NOT EXISTS STUDENT (" +
+            "first_name VARCHAR(30) NOT NULL," +
+            "middle_name VARCHAR(30)," +
+            "last_name VARCHAR(30) NOT NULL," +
+            "email VARCHAR(60) NOT NULL," +
+            "phone_number VARCHAR(12) UNIQUE," +
+            "home_number VARCHAR(12) NOT NULL," +
+            "year VARCHAR(1) NOT NULL," +
+            "password VARCHAR(12) NOT NULL UNIQUE," +
+            "program_id VARCHAR(10) NOT NULL," +
+            "FOREIGN KEY (program_id) REFERENCES PROGRAM(program_id)" +
+            " )";
+
+        try (Statement statement = connection.createStatement()) {
+            statement.executeUpdate(sqlProgram);
+            statement.executeUpdate(sqlStudent);
+        } catch (SQLException e) {
+            System.err.println("SQLException: " + e.getMessage());
+            System.exit(1);
+        } catch (Exception e) {
+            System.err.println("Exception: " + e.getMessage());
+            System.exit(1);
+        }
+    }
+}
