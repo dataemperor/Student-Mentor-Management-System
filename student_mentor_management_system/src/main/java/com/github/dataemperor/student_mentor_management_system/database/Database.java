a148b9744fcd7c2bb540560c83b3b2e086f54f3c
Thantulage Jayathu Chankama Fernando <155064611+dataemperor@users.noreply.github.com>
2025-07-31 19:42:36 +0530

    Student validation from database

--- student_mentor_management_system/src/main/java/com/github/dataemperor/student_mentor_management_system/database/Database.java
+++ student_mentor_management_system/src/main/java/com/github/dataemperor/student_mentor_management_system/database/Database.java
@@ -1,5 +1,7 @@
 package com.github.dataemperor.student_mentor_management_system.database;
 
+import com.github.dataemperor.student_mentor_management_system.classes.Program;
+import com.github.dataemperor.student_mentor_management_system.classes.Student;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -10,6 +12,15 @@ import java.sql.Statement;
 public class Database {
 
     private Connection connection = null;
+    String[][] programs = {
+        { "CM2601", "Object Orientated Development" },
+        { "CM2602", "Artificial Intelligence" },
+        { "CM2603", "Data Science Group Project" },
+        { "CM2604", "Machine Learning" },
+        { "CM2605", "Simulation and Modelling Techniques" },
+        { "CM2606", "Data Engineering" },
+        { "CM2607", "Advanced Mathematics for Data Science" },
+    };
 
     public Database() {
         try {
@@ -50,10 +61,12 @@ public class Database {
             "CREATE TABLE IF NOT EXISTS PROGRAM (" +
             "program_id VARCHAR(10) UNIQUE NOT NULL," +
             "program_name VARCHAR(30) NOT NULL," +
+            "program_year INTEGER NOT NULL" +
             "PRIMARY KEY (program_id)" +
             " )";
         String sqlStudent =
             "CREATE TABLE IF NOT EXISTS STUDENT (" +
+            "student_id VARCHAR(10) NOT NULL," +
             "first_name VARCHAR(30) NOT NULL," +
             "middle_name VARCHAR(30)," +
             "last_name VARCHAR(30) NOT NULL," +
@@ -63,41 +76,163 @@ public class Database {
             "year VARCHAR(1) NOT NULL," +
             "password VARCHAR(12) NOT NULL UNIQUE," +
             "program_id VARCHAR(10) NOT NULL," +
+            "is_mentored BOOLEAN NOT NULL," +
             "FOREIGN KEY (program_id) REFERENCES PROGRAM(program_id)" +
             " )";
 
         try (Statement statement = connection.createStatement()) {
             statement.executeUpdate(sqlProgram);
             statement.executeUpdate(sqlStudent);
-
-            // populating the program table
-            String[][] programs = {
-                { "CM2601", "Object Orientated Development" },
-                { "CM2602", "Artificial Intelligence" },
-                { "CM2603", "Data Science Group Project" },
-                { "CM2604", "Machine Learning" },
-                { "CM2605", "Simulation and Modelling Techniques" },
-                { "CM2606", "Data Engineering" },
-                { "CM2607", "Advanced Mathematics for Data Science" },
-            };
-
-            PreparedStatement preparedStatement = connection.prepareStatement(
-                "INSERT OR IGNORE INTO PROGRAM (program_id, program_name) VALUES (?, ?)"
-            );
-
-            for (String[] program : programs) {
-                preparedStatement.setString(1, program[0]);
-                preparedStatement.setString(2, program[1]);
-                preparedStatement.executeUpdate();
-            }
-
-            preparedStatement.close();
         } catch (SQLException e) {
             System.err.println("SQLException: " + e.getMessage());
             System.exit(1);
         } catch (Exception e) {
             System.err.println("Exception: " + e.getMessage());
             System.exit(1);
+        }
+    }
+
+    boolean placeholder = false;
+
+    // checks if the email is in the database
+    public boolean validateEmail(String email) throws SQLException {
+        String selectEmailQuery =
+            "SELECT COUNT(*) FROM STUDENT WHERE email = ?";
+
+        try (
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                selectEmailQuery
+            );
+        ) {
+            preparedStatement.setString(1, email);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt(1) > 0;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    // checks if the password is a new password
+    public boolean validatePassword(String password) throws SQLException {
+        String selectPasswordQuery =
+            "SELECT COUNT(*) FROM STUDENT WHERE password = ?";
+
+        try (
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                selectPasswordQuery
+            );
+        ) {
+            preparedStatement.setString(1, password);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt(1) > 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    // validates program id, name and year
+    public boolean validateProgram(Program program) {
+        String programId = program.getProgramId();
+        String programName = program.getProgramName();
+        int programYear = program.getProgramYear();
+        boolean validProgramId = false;
+        boolean validProgramName = false;
+        boolean validProgramYear = false;
+
+        for (String[] presetProgram : programs) {
+            if (presetProgram[0] == programId) {
+                validProgramId = true;
+            }
+
+            if (presetProgram[1] == programName) {
+                validProgramName = true;
+            }
+
+            if (programYear > 0 && programYear < 5) {
+                validProgramYear = true;
+            }
+        }
+
+        if (validProgramId && validProgramName && validProgramYear) {
+            return true;
+        }
+
+        return false;
+    }
+
+    // checks if the phone number is in the database
+    public boolean validatePhoneNumber(String phoneNumber) throws SQLException {
+        String selectPhoneNumberQuery =
+            "SELECT COUNT(*) FROM STUDENT WHERE phone_number = ?";
+
+        try (
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                selectPhoneNumberQuery
+            );
+        ) {
+            preparedStatement.setString(1, phoneNumber);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt(1) > 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    // checks if the student id is in the database
+    public boolean validateStudentId(String studentId) throws SQLException {
+        String selectStudentIdQuery =
+            "SELECT COUNT(*) FROM STUDENT WHERE student_id = ?";
+
+        try (
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                selectStudentIdQuery
+            );
+        ) {
+            preparedStatement.setString(1, studentId);
+
+            try (ResultSet resultSet = preparedStatement.executeQuery()) {
+                if (resultSet.next()) {
+                    return resultSet.getInt(1) > 0;
+                }
+            }
+        }
+        return false;
+    }
+
+    public void insertStudent(Student student) throws SQLException {
+        String insertStudentQuery =
+            "INSERT INTO STUDENT (" +
+            "first_name, middle_name, last_name," +
+            "email, phone_number, home_number" +
+            "year, password, program_id" +
+            ") VALUES (?, ?, ?, ?, ?, ?, ?,?, ?);";
+
+        try (
+            PreparedStatement preparedStatement = connection.prepareStatement(
+                insertStudentQuery
+            );
+        ) {
+            preparedStatement.setString(1, student.getFirstName());
+            preparedStatement.setString(2, student.getMiddleName());
+            preparedStatement.setString(3, student.getLastName());
+            preparedStatement.setString(4, student.getEmailAddress());
+            preparedStatement.setString(5, student.getContactNumber());
+            preparedStatement.setString(6, student.getHomeNumber());
+            preparedStatement.setInt(7, student.getProgram().getProgramYear());
+            preparedStatement.setString(8, student.getPassword());
+            preparedStatement.setString(9, student.getProgram().getProgramId());
+
+            preparedStatement.executeUpdate();
         }
     }
 }
